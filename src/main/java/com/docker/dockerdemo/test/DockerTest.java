69afa38ffdfabbdd8178276dacbbf47a67d3b27f
zxl <1378248073@qq.com>
2018-03-05 16:04:52 +0800

    update

---
+++ src/main/java/com/docker/dockerdemo/test/DockerTest.java
@@ -0,0 +1,10 @@
+package com.docker.dockerdemo.test;
+
+public class DockerTest {
+
+    public String gitDemo(){
+        String str="test git demo";
+        System.out.println(str);
+        return str;
+    }
+}
--- src/test/java/com/docker/dockerdemo/DockerDemoApplicationTests.java
+++ src/test/java/com/docker/dockerdemo/DockerDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.docker.dockerdemo;
 
+import com.docker.dockerdemo.test.DockerTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,12 @@ public class DockerDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void gitTest(){
+		DockerTest dockerTest = new DockerTest();
+		dockerTest.gitDemo();
 	}
 
 }
